a0275a5d847b27562047045316706ea913718408
KezroXV <kezro10@gmail.com>
2025-10-14 20:31:25 +0100

    Import Image component in Header for logo support: prepare for future enhancements related to logo display and customization options.

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,6 +14,7 @@ import {
   ChevronDown,
 } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useCurrentUser } from "@/lib/hooks/useCurrentUser";
 import { usePermissions } from "@/lib/hooks/usePermissions";
